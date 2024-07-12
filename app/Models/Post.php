e3b796923d812bc06faeea325b9496cd3725f13e
RonaldBeltran1222 <alexanderbeltran2208@outlook.com>
2024-07-12 18:50:32 -0500

    realciones de con la base de datos creada

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //RELACION UNO A MUCHOS INVERSA
+    public function user(){
+        return $this->belongsTo('User::class');
+    }
+
+    public function category(){
+        return $this->belongsTo('Category::class');
+    }
+
+    //RELACION MUCHOS A MUCHOS
+    public function tags(){
+        return $this->belongsToMany('Tag::class');
+    }
+
+    //RELACION UNO A UNO POLIMORFICA
+    public function image(){
+        return $this->morphTo(Image::class, 'imageable');
+    }
+}
